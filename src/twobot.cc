800e722f26d91a854d0ec39b46631443050e338a
Root <3416616002@qq.com>
2022-07-26 17:42:42 +0800

    实现大体框架

--- src/twobot.cc
+++ src/twobot.cc
@@ -1,4 +1,19 @@
-//
-// Created by Root on 2022/07/26.
-//
+#include "twobot.hh"
 
+namespace twobot{
+  std::unique_ptr<BotInstance> BotInstance::createInstance(const Config &config) {
+        return std::make_unique<BotInstance>(config);
+  }
+
+    ApiSet &BotInstance::getApiSet() {
+        return this->apiSet;
+    }
+
+    BotInstance::BotInstance(const Config &config) :
+        apiSet(ApiSet{
+            //TODO: 创建初始化ApiSet的代码
+        })
+    {
+
+    }
+};
--- src/twobot.hh
+++ src/twobot.hh
@@ -1,5 +1,31 @@
-//
-// Created by Root on 2022/07/26.
-//
-
 #pragma once
+#include <string>
+#include <string_view>
+#include <optional>
+#include <memory>
+
+namespace twobot {
+    struct Config{
+        std::string url;
+        std::uint16_t  port;
+        std::optional<std::string> token;
+    };
+
+    // Api集合，所有对机器人调用的接口都在这里
+    struct ApiSet{
+//TODO: 实现ApiSet
+    };
+
+    /// BotInstance是一个机器人实例，机器人实例必须通过BotInstance::createInstance()创建
+    /// 因为采用了unique_ptr，所以必须通过std::move传递，可以99.99999%避免内存泄漏
+    struct BotInstance{
+        static std::unique_ptr<BotInstance> createInstance(const Config &config);
+        ApiSet& getApiSet();
+    protected:
+        ApiSet apiSet;
+        explicit BotInstance(const Config &config);
+        ~BotInstance() = default;
+        friend std::unique_ptr<BotInstance> std::make_unique<BotInstance>(const Config &config);
+        friend std::default_delete<BotInstance>;
+    };
+};
